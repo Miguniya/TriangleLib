500abfed54cb4072caf5338ae57dccb62aa5fb07
Anastasiia <58131714+Miguniya@users.noreply.github.com>
2022-06-02 15:30:32 +0300

    Add files via upload

---
+++ testForDll/testForDll/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testForDll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TriangleLibrary.TriangleLib.getTriangleType(5, 5, 4).ToString());
+            Console.ReadKey();
+        }
+    }
+}
